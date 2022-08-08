1796cbf08ba579a2009031b367d83ad22624aced
Tomas <tomibudeguer20@gmail.com>
2022-08-07 21:30:31 -0300

    mejore el ejercicio, mostrando los datos tanto en consola como en pantalla con createElement

--- js/app.js
+++ js/app.js
@@ -1,20 +1,27 @@
 let tareasNuevas = [];
 function agregarTarea() {
   let tareaBuscada = document.querySelector("#tarea").value;
-  console.log(tareaBuscada)
-  tareasNuevas.push(tareaBuscada)
-  console.log(tareasNuevas)
+  console.log(tareaBuscada);
+  tareasNuevas.push(tareaBuscada);
+  console.log(tareasNuevas);
+  let sectionPadre = document.querySelector("#sectionPadre");
+  let parrafoNuevoAgregar = document.createElement("p");
+  parrafoNuevoAgregar.innerHTML = `Tareas: ${tareasNuevas} (se agrego: ${tareaBuscada})`;
+  sectionPadre.appendChild(parrafoNuevoAgregar);
 }
 
 function eliminarTarea() {
-    let tareaBuscada = document.querySelector("#tarea").value;
-    console.log(tareaBuscada)
-    let tareaBorro = tareasNuevas.indexOf(tareaBuscada)
-    console.log(tareaBorro)
-    if(tareaBorro > -1){
-        tareasNuevas.splice(tareaBorro,1)
-        console.log(tareasNuevas)
-    }
+  let tareaBuscada = document.querySelector("#tarea").value;
+  console.log(tareaBuscada);
+  let tareaBorro = tareasNuevas.indexOf(tareaBuscada);
+  if (tareaBorro > -1) {
+    tareasNuevas.splice(tareaBorro, 1);
+    console.log(tareasNuevas);
+    let sectionPadre = document.querySelector("#sectionPadre");
+    let parrafoTareaBorrar = document.createElement("p");
+    parrafoTareaBorrar.innerHTML = `Tareas: ${tareasNuevas} (se borro: ${tareaBuscada})`;
+    sectionPadre.appendChild(parrafoTareaBorrar);
+  }else{
+    alert('No existe esa Tarea')
+  }
 }
-
-
